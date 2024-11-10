aa278ef52a86cf51904a8615ad65691939275277
Ukeshvaran <ukeshvaranuki123@gmail.com>
2024-11-11 02:38:35 +0530

    Update union.py

--- union/union.py
+++ union/union.py
@@ -34,7 +34,7 @@ except Exception as e:
     st.error(f"Error connecting to Database: {e}")
 try:
     #st.columns(3)[1].title(":red[Welcome] :balloon:")
-    st.columns(5)[2].image("https://stickerdise.com/wp-content/uploads/2022/05/SINGLE42_kiss-cut-stickers-5.5x5.5-default-626e86e97eb7d.webp",width=180)
+    st.columns(5)[2].image("https://stickerdise.com/wp-content/uploads/2022/05/SINGLE42_kiss-cut-stickers-5.5x5.5-default-626e86e97eb7d.webp",width=250)
     def info():
             name = st.text_input("Name :",placeholder="Enter your name")
     
