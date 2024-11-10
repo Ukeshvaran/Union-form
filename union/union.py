da84290fdf691ba801cb1da00fb500b5852dcf4e
Ukeshvaran <ukeshvaranuki123@gmail.com>
2024-11-11 01:41:20 +0530

    Update union.py

--- union/union.py
+++ union/union.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import psycopg2 as pg
 import urllib.parse as urlparse
+import time
 
 st.markdown(
     """
@@ -76,8 +77,11 @@ try:
     
                  submit = st.columns(4)[2].button("Submit",type='primary')
                  if submit:
+                     with st.spinner(text="Loading....."):
+                         time.sleep(2)
                      st.success("Thanks for your response!", icon=":material/thumb_up:")
-                     st.error("You can now close your browser....")
+                     st.error("You can now close your browser")
+                     
                      sqlupdate(name, phone ,theme,event_avail,availability,eve_plc, food, suggestion,performance)
                      st.balloons()
             st.markdown(":copyright: :blue[yukesh] personal deployment")
