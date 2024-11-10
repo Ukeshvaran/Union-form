86031ee57f5778efd0a10d4ea3d1cd27b25a3d63
Ukeshvaran <ukeshvaranuki123@gmail.com>
2024-11-11 02:42:48 +0530

    Update union.py

--- union/union.py
+++ union/union.py
@@ -70,6 +70,7 @@ try:
                       conn=st.container(border=True)
                       conn.write("if your going to perform any group activity. Kindly coordinate with your team and update here, what your going to perform along with your team-mates name")
                  performance=st.text_input("Please enter what you are going to perform :",placeholder="Enter your input in a single line")
+            st.divider()
             check_box=st.checkbox("click & submit")
             if check_box:
                  with st.chat_message('ai'):
