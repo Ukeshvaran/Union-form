03ea7feaf08311d91f54bf1fb5b2cd2d7caa1bed
Ukeshvaran <ukeshvaranuki123@gmail.com>
2024-11-12 13:18:50 +0530

    Update union.py

--- union/union.py
+++ union/union.py
@@ -108,7 +108,7 @@ try:
                     container.write("This information will be stored in a cloud database that can be accessed whenever necessary. It will be fully encrypted from end to end, ensuring that only authorized personnel and the admin can access it, keeping it secure from unauthorized users.")
             
                  with col2:
-                       submit = st.columns(3)[1].button("Submit",type='primary')
+                       submit = st.button("Submit",type='primary')
              
                  if submit:
                      with st.spinner(text="Loading......"):
